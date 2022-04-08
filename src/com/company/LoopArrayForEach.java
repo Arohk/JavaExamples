a74a8dc459163e84d07888fdc71917322dfe4e4c
Nikolay Nedyalkov <Nikolay_Nedyalkov@starsgroup.com>
2022-04-08 12:49:10 +0300

    initial commit

---
+++ src/com/company/LoopArrayForEach.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class LoopArrayForEach {
+    public static void main(String args[]){
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+        for (String carName : cars) {
+            System.out.println(carName);
+        }
+
+    }
+
+}
